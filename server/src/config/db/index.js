409368a679de54cf86e41b75a45034e48cb78530
thong2085 <129254706+thong2085@users.noreply.github.com>
2023-10-11 11:03:27 +0700

    fix connect mongoAlats

--- server/src/config/db/index.js
+++ server/src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect(){
 
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/HRM_KDE');
+        await mongoose.connect(`${process.env.mongo_db}}`);
         console.log('Kết nối thành công!!');
     } catch (error) {
         console.log('Kết nối thất bại!!');
